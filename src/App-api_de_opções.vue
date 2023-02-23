43b3c2f3c48cffd73705fb438a7660766da2b6dc
Marco André Mendes <marcoandre@gmail.com>
2023-02-23 15:34:37 -0300

    api de opçao e composição

---
+++ src/App-api_de_opções.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+    incrementarContador() {
+      this.contador++
+    },
+    decrementarContador() {
+      if (this.contador > 0) this.contador--
+    },
+    resetarContador() {
+      this.contador = 0
+    }
+  },
+  mounted() {
+    console.log(`O valor inicial do contador é ${this.contador}.`)
+  }
+}
+</script>
+
+<template>
+  <button @click="incrementarContador">Incrementar</button>
+  <button @click="decrementarContador">Decrementar</button>
+  <button @click="resetarContador">resetarContador</button>
+  <p>Valor do contador é: {{ contador }}</p>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
