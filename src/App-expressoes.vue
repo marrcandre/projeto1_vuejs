0d67fba4aaecf2e61678639f56b4df52c2a6cb8f
Marco André Mendes <marcoandre@gmail.com>
2023-02-23 16:04:45 -0300

    templates com expressões e funções

---
+++ src/App-expressoes.vue
@@ -0,0 +1,17 @@
+<script setup>
+const numero = 1
+const ok = true
+const mensagem = 'Vue3'
+</script>
+
+<template>
+  {{ Math.pow(numero + 1, 2) }}
+  {{ ok ? 'Sim' : 'Não' }}
+  {{ mensagem.split('').reverse().join('') }}
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
