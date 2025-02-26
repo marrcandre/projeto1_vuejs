0d88fd026e6554ad03aab090dfedc03530e2748b
Marco André Mendes <marcoandre@gmail.com>
2025-02-26 14:55:31 -0300

    Organizando e renomeando arquivos.

---
+++ src/App-00b.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+const texto = ref('Digite seu nome')
+</script>
+
+<template>
+  <div>
+      <input type="text" v-model="texto" />
+      <p>Texto digitado: {{ texto }}</p>
+  </div>
+</template>
